148e201ab582a49068778b871f89a1f2993b030d
xudaquan2003 <xudaquan2003@gmail.com>
2025-01-26 09:26:19 +0800

    fix bug:op-reth dockerfile

--- clients/op-reth/entrypoint.sh
+++ clients/op-reth/entrypoint.sh
@@ -1,20 +1,25 @@
 #!/bin/sh
 set -exu
 
+if [ -f /genesis_account.json ]; then
+    jq -s '.[0] * .[1]' /genesis_account.json /genesis.json  > /genesis_tmp.json
+    mv /genesis_tmp.json /genesis.json
+fi
+
 op-reth node \
     --datadir=/data/op-reth/execution-data \
-    --chain=/genesis-2151908.json \
+    --chain=/genesis.json \
     --http \
-    --http.port=9545 \
+    --http.port=$HIVE_L2_HTTP_PORT \
     --http.addr=0.0.0.0 \
     --http.corsdomain=* \
     --http.api=admin,net,eth,web3,debug,trace,miner \
     --ws \
     --ws.addr=0.0.0.0 \
-    --ws.port=9546 \
+    --ws.port=$HIVE_L2_WS_PORT \
     --ws.api=admin,net,eth,web3,debug,trace,miner \
     --ws.origins=* \
-    --authrpc.port=9551 \
+    --authrpc.port=$HIVE_L2_AUTH_PORT \
     --authrpc.jwtsecret=/jwtsecret \
     --authrpc.addr=0.0.0.0 \
     --discovery.port=30303 \
